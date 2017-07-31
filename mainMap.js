a8391d3bc70df79770d51b8f76fb6f4a11fec074
Bill Morris <wboykinm@geosprocket.com>
2017-07-30 22:25:03 -0400

    began trail of rage commits re: popups

---
+++ mainMap.js
@@ -0,0 +1,126 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2lqbmpqazdlMDBsdnRva284cWd3bm11byJ9.V6Hg2oYJwMAxeoR9GEzkAA';
+var url = "https://spreadsheets.google.com/feeds/list/1Qs0kZqTtlaMqgmqQphKmk7a0BELjQXeYb0CJVQZk7js/1/public/values?alt=json"
+// General function
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function() {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+// handle navigation
+function navMe(lon,lat) {
+  // if no geo in browser, use btv center
+  if (!navigator.geolocation){
+    var navUrl = 'nav?start=' + '-73.21349' + ',' + '44.47657' + '&end=' + lon + ',' + lat
+    window.open(navUrl)
+  } else {
+    // or use browser location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var navUrl = 'nav?start=' + position.coords.longitude + ',' + position.coords.latitude + '&end=' + lon + ',' + lat
+      window.open(navUrl)
+    },function error() {
+      // again, if no geo in browser, use btv center
+      var navUrl = 'nav?start=' + '-73.21349' + ',' + '44.47657' + '&end=' + lon + ',' + lat
+      window.open(navUrl)
+    });
+  }
+}
+
+$.getJSON(url, function(data) {
+  var stops = data.feed.entry;
+  
+  // build geojson from google sheet data:
+  var fountGeojson = {type: 'FeatureCollection', features: []};
+
+  for (var i in stops) {
+    if (stops[i].gsx$latitude.$t) {
+      var feature = {
+        type: 'Feature',
+        properties: {
+          name: stops[i].gsx$name.$t
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            stops[i].gsx$longitude.$t, 
+            stops[i].gsx$latitude.$t
+          ]
+        }
+      };
+      fountGeojson.features.push(feature);
+    }
+  }
+  
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v10',
+    center: [-73.21349,44.47657],
+    zoom: 14,
+    pitch: 55
+  });
+
+  // This adds the data to the map
+   map.on('load', function(e) {
+     map.addControl(new mapboxgl.NavigationControl());
+     map.addSource("places", {
+       "type": "geojson",
+       "data": fountGeojson
+     });
+     
+    // This is where your interactions with the symbol layer used to be
+    // Now you have interactions with DOM markers instead
+    fountGeojson.features.forEach(function(marker, i) {
+      // Create an img element for the marker
+      var el = document.createElement('div');
+      el.id = "marker-" + i;
+      el.style.left = '-18px';
+      el.style.top = '-18px';
+      el.className = 'marker'// ' + typeLookup[marker.properties.name];
+      // Add markers to the map at all points
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+
+      el.addEventListener('click', function(e) {
+        // 1. Fly to the point
+        flyToSite(marker);
+        // 2. Close all other popups and display popup for clicked site
+        createPopUp(marker);
+      });
+    });
+
+    // add wacky movement because you're still not over all the 
+    // 3d stuff available w/ GL
+    function flyToSite(currentFeature) {
+      map.flyTo({
+        center: currentFeature.geometry.coordinates,
+        zoom: 15,
+        bearing: Math.floor(Math.random() * 50) + 1,
+        pitch: Math.floor(Math.random() * 40) + 21,
+        speed: 0.3,
+        curve: 1
+      });
+    }
+
+    function createPopUp(currentFeature) {
+      var popUps = document.getElementsByClassName('mapboxgl-popup');
+      if (popUps[0]) popUps[0].remove();
+      var popupContent = '<h1>' + currentFeature.properties.name + '</h1>'
+      /*if (currentFeature.properties.description) {
+        popupContent = '<h3>' + currentFeature.properties.name + '</h3>' + '<h4>' + currentFeature.properties.description + '</h4><p class="pad3"><a onclick="navMe(' + currentFeature.geometry.coordinates + ')" class="button">Navigate</a></p>'
+      } else {
+        popupContent = '<h3>' + currentFeature.properties.name + '</h3><p class="pad3"><a onclick="navMe(' + currentFeature.geometry.coordinates + ')" class="button">Navigate</a></p>'
+      }*/
+      //console.log(popupContent)
+      var popup = new mapboxgl.Popup({
+          closeOnClick: true
+        })
+        .setLngLat(currentFeature.geometry.coordinates)
+        .setHTML(popupContent)
+        .addTo(map);
+      console.log(popup)
+    }
+  });
+});
